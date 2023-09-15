c988941fbcb1806aee7ccc628c1f4f9fdd076499
Dani <faucin@gmail.com>
2023-09-15 01:37:08 -0300

    removes unnecesary things

--- src/win32_main.h
+++ src/win32_main.h
@@ -1,8 +1,6 @@
 #ifndef WIN32_MAIN_H
 #include "definitions.h"
-#include <dsound.h>
 #include <windows.h>
-#include <xinput.h>
 
 struct Win32OffscreenBuffer {
   BITMAPINFO info;
@@ -16,23 +14,6 @@ struct Win32WindowDimension {
   int width;
   int height;
 };
-
-struct Win32SoundOutput {
-  int samplesPerSecond;
-  int bytesPerSample;
-  int wavePeriod;
-  int secondaryAudioBufferSize;
-  int toneVolume;
-  uint32 runningSampleIndex;
-  int16 toneHz;
-};
-
-struct Win32SoundConfig {
-  DWORD bytesToWrite;
-  DWORD byteToLock;
-  bool32 succeeded;
-};
-
 
 
 #define WIN32_MAIN_H
